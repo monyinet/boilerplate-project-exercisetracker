2fe27d25f4f6557e00fe366bf906c7663cb4b83c
Jose Juan Moñino Coll <36202313+monyinet@users.noreply.github.com>
2022-12-01 19:41:46 +0000

    install lowdb and configure it

--- index.js
+++ index.js
@@ -10,11 +10,27 @@ import { fileURLToPath } from 'node:url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const file = join(__dirname, 'db.json');
 
+import { Low } from 'lowdb';
+import { JSONFile } from 'lowdb/node';
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+// Configure lowdb to write to JSONFile
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+await db.read();
+
+db.data ||= {
+	users: [],
+	exercises: [],
+	log: []
+};
+const { urls } = db.data;
+await db.write();
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
