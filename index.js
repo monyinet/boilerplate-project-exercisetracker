643c529295036dbaf8d77ecd92c190b4b8e046ef
Jose Juan Moñino Coll <36202313+monyinet@users.noreply.github.com>
2022-12-01 19:29:55 +0000

    change to type module and install packages

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+import { config } from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import { uid } from 'uid';
+import morgan from 'morgan';
+const app = express();
+
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'db.json');
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
-
-
-
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
